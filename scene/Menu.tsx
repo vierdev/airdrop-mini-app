7e0d553959169027cfb2f2c6a0bbffd665e7ee20
vierdev <jamiemmacl@outlook.com>
2024-12-14 11:50:17 -0800

    fix: deployment path error

--- scene/Menu.tsx
+++ scene/Menu.tsx
@@ -1,8 +1,8 @@
 "use client"
 
 import React, { useEffect, useState } from "react";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import { usePathname } from "next/navigation";
 
 interface MenuTemplateProps {
